e7a0b8379b38bce2a9689cfe584ceeec647a3e92
Beth Jackson <Mbeth.ashton@gmail.com>
2020-10-16 12:30:22 -0400

    Use Behaviour for Genes

--- lib/wyvern/gene.ex
+++ lib/wyvern/gene.ex
@@ -1,4 +1,9 @@
 defmodule Wyvern.Gene do
+  @callback phenotype(arg :: list(any)) :: bitstring() # one or two word description of result
+  @callback new(arg :: list(any), list(any)) :: list(any) # if you have two parents, use Wyvern.Gene.new(value1, value2)
+  @callback new() :: list(any) # if it is wild caught, define your defaults. 
+  
+  @spec new([...], [...]) :: [...]
   def new([a, b], [c, d]) do
     pick = Enum.random(1..4)
     case pick do
---
+++ lib/wyvern/genes/gender.ex
@@ -0,0 +1,15 @@
+defmodule Wyvern.Genes.Gender do
+  @behaviour Wyvern.Gene
+  def phenotype(gene) do
+    case gene do
+      [:x, :y] -> "male"
+      [:x, :x] -> "female"
+    end
+  end
+  def new(gene1, gene2) do
+    Wyvern.Gene.new(gene1, gene2)
+  end
+  def new() do
+    Wyvern.Gene.new([:x, :x], [:x, :y])
+  end
+end
